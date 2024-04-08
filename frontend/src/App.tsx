e7f331b7f1cba09e0fa53085c25c1b5c3637647e
Piotr Świebocki <swpiotr5@interia.pl>
2024-04-08 19:46:27 +0200

    (frontend) routing for specific view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "./container/Home";
 import Login from "./container/Login";
 import Register from "./container/Register";
+import Pantry from "./container/Pantry";
+import PizzaCreator from "./container/PizzaCreator";
+import PizzaMetrics from "./container/PizzaMetrics";
+import Profile from "./container/Profile";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path="/register" element={<Register />} />
+            <Route path='/pantry' element={<Pantry />} />
+            <Route path='/pizzacreator' element={<PizzaCreator />} />
+            <Route path='/pizzametrics' element={<PizzaMetrics />} />
+            <Route path='/profile' element={<Profile />} />
         </Routes>
       </>
   );
