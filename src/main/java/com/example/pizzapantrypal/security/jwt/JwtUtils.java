c4ef3796698972442cd19ec8e77924e5c5221623
Piotr Świebocki <swpiotr5@interia.pl>
2024-05-04 01:00:33 +0200

    (backend) fixed jwt

--- src/main/java/com/example/pizzapantrypal/security/jwt/JwtUtils.java
+++ src/main/java/com/example/pizzapantrypal/security/jwt/JwtUtils.java
@@ -2,6 +2,7 @@ package com.example.pizzapantrypal.security.jwt;
 import java.security.Key;
 import java.util.Date;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,30 +24,42 @@ public class JwtUtils {
     @Value("${app.jwtExpirationInMs}")
     private int jwtExpirationMs;
 
-    public String generateJwtToken(Authentication authentication) {
+    private Key key;
 
+    @PostConstruct
+    public void init() {
+        // Decode the secret key from Base64
+        byte[] decodedKey = Decoders.BASE64.decode(jwtSecret);
+
+        // If the key is not long enough, generate a new one
+        if (decodedKey.length < 32) {
+            logger.warn("JWT secret key length is less than 256 bits. Generating a new key...");
+            key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+            logger.warn("New JWT secret key generated.");
+        } else {
+            key = Keys.hmacShaKeyFor(decodedKey);
+        }
+    }
+
+    public String generateJwtToken(Authentication authentication) {
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
         return Jwts.builder()
-                .setSubject((userPrincipal.getUsername()))
+                .setSubject(userPrincipal.getUsername())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(key(), SignatureAlgorithm.HS256)
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    private Key key() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
-    }
-
     public String getUserNameFromJwtToken(String token) {
-        return Jwts.parserBuilder().setSigningKey(key()).build()
+        return Jwts.parserBuilder().setSigningKey(key).build()
                 .parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateJwtToken(String authToken) {
         try {
-            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            Jwts.parserBuilder().setSigningKey(key).build().parse(authToken);
             return true;
         } catch (MalformedJwtException e) {
             logger.error("Invalid JWT token: {}", e.getMessage());
