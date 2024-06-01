8a5fbf30a5ad50965aa60027e939c2fdf791a43a
Piotr Świebocki <swpiotr5@interia.pl>
2024-06-01 11:18:27 +0200

    (frontend): mobile navbar feature - pizzacreator not visible for user

--- frontend/src/components/DefaultTemplate/MobileNavbar.tsx
+++ frontend/src/components/DefaultTemplate/MobileNavbar.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { createUseStyles } from 'react-jss';
 import { FaUser, FaStore, FaBalanceScale, FaPizzaSlice } from 'react-icons/fa';
 import NavigationItem from './NavigationItem';
 import { useLocation } from 'react-router-dom';
+import axios from "axios";
 
 const useStyles = createUseStyles({
     mobileNavbar: {
@@ -32,16 +33,60 @@ const useStyles = createUseStyles({
     },
 });
 
+interface User {
+    username: string;
+    email: string;
+    roles: { name: string }[];
+}
+
+interface Role {
+    name: string;
+}
+
+
 const MobileNavbar: React.FC = () => {
     const classes = useStyles();
     const location = useLocation();
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const token = localStorage.getItem('access_token');
+                const response = await axios.get('http://localhost:8080/api/user/current', {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                let userData = response.data;
+                if (userData) {
+                    userData.roles = userData.roles.map((role: Role) => {
+                        if (role.name === 'ROLE_MANAGER') {
+                            return { name: 'manager' };
+                        } else if (role.name === 'ROLE_USER') {
+                            return { name: 'user' };
+                        } else {
+                            return role;
+                        }
+                    });
+                }
+                setUser(userData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchUser();
+    }, []);
 
     return (
         <div className={classes.mobileNavbar}>
             <NavigationItem to="/profile" label={<FaUser className={location.pathname === "/profile" ? classes.activeNavItem : classes.navItem}/>} />
             <NavigationItem to="/pantry" label={<FaStore className={location.pathname === "/pantry" ? classes.activeNavItem : classes.navItem}/>} />
             <NavigationItem to="/pizzametrics" label={<FaBalanceScale className={location.pathname === "/pizzametrics" ? classes.activeNavItem : classes.navItem}/>} />
-            <NavigationItem to="/pizzacreator" label={<FaPizzaSlice className={location.pathname === "/pizzacreator" ? classes.activeNavItem : classes.navItem}/>} />
+            {user && user.roles.some(role => role.name === 'manager') && (
+                <NavigationItem to="/pizzacreator" label={<FaPizzaSlice className={location.pathname === "/pizzacreator" ? classes.activeNavItem : classes.navItem}/>} />
+            )}
         </div>
     );
 };
